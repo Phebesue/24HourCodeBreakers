59cc924dde50b4767a1d2f62d3c8aeeae60b2200
Fletcher Kitchell <fdkitchell@gmail.com>
2020-06-02 09:35:11 -0400

    Added like classes, properties, and services. changed namespace to plural posts.

---
+++ ClassB.Services/LikeServices.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassB.Services
+{
+    public class LikeService
+    {
+        private readonly Guid _userId;
+        public LikeService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateLike(LikeCreate model)
+        {
+            var entity =
+                new Like()
+                {
+                    UserId = _userId,
+                    Comment = model.Comment,
+                    Post = model.Post,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Likes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<LikeListItem> GetLikes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Likes
+                    .Where(e => e.Author.UserId == _userId)
+                    .Select(e => new LikeListItem
+                    {
+                        Author = e.Author,
+                        Comment = e.Comment,
+                        Post = e.Post,
+                    }
+                    );
+                return query.ToArray();
+            }
+        }
+    }
+}
